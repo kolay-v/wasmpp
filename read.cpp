7eea1bcab07f6c161dc721a8d2b450524962b4c5
@kolayv <kolayv@pm.me>
2019-07-18 19:17:33 +0000

    added pch and awsome read uint32 function

--- read.cpp
+++ read.cpp
@@ -1,6 +1,8 @@
-#include<iostream>
-#include<vector>
-#include<fstream>
+#include"pc.h"
+
+uint32_t read(std::vector<uint8_t> buffer, u_int32_t i) {
+  return (uint32_t)(buffer[i * 4] | buffer[i * 4 + 1] << 8 | buffer[i * 4 + 2] << 16 | buffer[i * 4 + 3] << 24);
+}
 
 int main () {
   std::ifstream file("test.wasm", std::ios::binary | std::ios::ate);
@@ -8,10 +10,7 @@ int main () {
   file.seekg(0, std::ios::beg);
   std::vector<uint8_t> buffer(size);
   if (file.read(reinterpret_cast<char*>(buffer.data()), size)) {
-    std::cout << std::hex << (uint32_t)(buffer[0] | buffer[1] << 8 | buffer[2] << 16 | buffer[3] << 24) << '\n';
-    std::cout << std::hex << (int)buffer[4];
-    std::cout << std::hex << (int)buffer[5];
-    std::cout << std::hex << (int)buffer[6];
-    std::cout << std::hex << (int)buffer[7] << '\n';
+    std::cout << std::hex << read(buffer, 0) << '\n';
+    std::cout << std::hex << read(buffer, 1) << '\n';    
   }
 }
