5ca952d392faa89d7295d4e2e75705316c7e3ea7
@kolayv <kolayv@pm.me>
2019-07-18 19:50:26 +0000

    Added header verification

--- read.cpp
+++ read.cpp
@@ -1,7 +1,11 @@
 #include"pc.h"
 
-uint32_t read(std::vector<uint8_t> buffer, u_int32_t i) {
+uint32_t read_u32(std::vector<uint8_t> buffer, u_int32_t i) {
   return (uint32_t)(buffer[i * 4] | buffer[i * 4 + 1] << 8 | buffer[i * 4 + 2] << 16 | buffer[i * 4 + 3] << 24);
+}
+
+bool verify_header (std::vector<uint8_t> buffer) {
+  return read_u32(buffer, 0) == 0x6d736100 && read_u32(buffer, 1) == 1;
 }
 
 int main () {
@@ -9,8 +13,14 @@ int main () {
   std::streamsize size = file.tellg();
   file.seekg(0, std::ios::beg);
   std::vector<uint8_t> buffer(size);
-  if (file.read(reinterpret_cast<char*>(buffer.data()), size)) {
-    std::cout << std::hex << read(buffer, 0) << '\n';
-    std::cout << std::hex << read(buffer, 1) << '\n';    
+  if (!file.read(reinterpret_cast<char*>(buffer.data()), size)) {
+    std::cout << "cant read file\n";
+    return 1;
+  }
+  if (verify_header(buffer)) {
+    std::cout << "succes";
+  } else {
+    std::cout << "invalid file format";
+    return 1;
   }
 }
